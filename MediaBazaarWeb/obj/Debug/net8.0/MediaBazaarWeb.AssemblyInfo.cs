8ed940298b0ee6a0c205df1cf5c3fae5e7a5ba1e
samuilkozarov <samuilkozarov@SAMUILKOZAR6649>
2024-09-15 12:02:31 +0200

    a

--- MediaBazaarWeb/obj/Debug/net8.0/MediaBazaarWeb.AssemblyInfo.cs
+++ MediaBazaarWeb/obj/Debug/net8.0/MediaBazaarWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaBazaarWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6935e939a360a487ee35117828e80cde1356e7a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+524706f98c4cc2cea73bf46961e0541e90e3cba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaBazaarWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaBazaarWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

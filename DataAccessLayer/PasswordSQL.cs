7105af7c2cbc2f987cd2830c329aa9461ac99525
samuilkozarov <samuilkozarov@SAMUILKOZAR6649>
2024-09-15 13:42:33 +0200

    Comments are added for the Password SQL class

--- DataAccessLayer/PasswordSQL.cs
+++ DataAccessLayer/PasswordSQL.cs
@@ -24,19 +24,28 @@ using Microsoft.VisualBasic;
 
 namespace DataAccessLayer
 {
+
+    /// <summary>
+    /// Provides methods for interacting with the password storage in the database, 
+    /// including retrieving, updating, and adding password hashes and salts.
+    /// </summary>
     public class PasswordSQL
     {
         private string connectionString = "Server=mssqlstud.fhict.local;Database=dbi527531_mediashop;User Id=dbi527531_mediashop;Password=mediashop123; TrustServerCertificate=True";
 
 
-
+        /// <summary>
+        /// Retrieves the password hash and salt for a specific user.
+        /// </summary>
+        /// <param name="id">The ID of the user whose password hash and salt are to be retrieved.</param>
+        /// <returns>A dictionary containing the password hash and salt for the specified user.</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with retrieving the password hash and salt from the database.</exception>
         public Dictionary<string, string> GetUserHashSalt(int id)
         {
             string query = $"SELECT passwordHash, passwordSalt FROM Passwords WHERE person_id = @id";
 
             Dictionary<string, string> hashSalt = new Dictionary<string, string>();
             SqlConnection connection = new SqlConnection(connectionString);
-
 
             try
             {
@@ -46,17 +55,8 @@ namespace DataAccessLayer
 
                 SqlDataReader reader = command.ExecuteReader();
 
-                // Allow the query to the be sent Database
-                //SqlCommand command = new SqlCommand(query, connection);
-
-                // Add the parameters
-
-                // Execute the query and get the data
-                //using SqlDataReader reader = command.ExecuteReader();
-
                 while (reader.Read())
                 {
-
                     hashSalt.Add("hash", (string)reader["passwordHash"]);
                     hashSalt.Add("salt", (string)reader["passwordSalt"]);
                     return hashSalt;
@@ -74,45 +74,21 @@ namespace DataAccessLayer
                 { connection.Close(); }
             }
         }
-        //public void AddPassHash(int id,string salt, string hash)
-        //{
-        //    string query = $"Insert into Passwords(person_id,passwordSalt,passwordHash) values (@id,@passwordSalt,@passwordHash)";
 
-        //    Dictionary<string, string> hashSalt = new Dictionary<string, string>();
-        //    SqlConnection connection = new SqlConnection(connectionString);
 
-        //    try
-        //    {
-        //        connection.Open();
-        //        SqlCommand command = new SqlCommand(query, connection);
-
-        //        // Add the parameters
-        //        command.Parameters.AddWithValue("@id", id);
-        //        command.Parameters.AddWithValue("@passwordSalt", salt);
-        //        command.Parameters.AddWithValue("@passwordHash", hash);
-        //        command.ExecuteNonQuery();
-
-        //        // Execute the query and get the data
-        //        //using SqlDataReader reader = command.ExecuteReader();
-
-        //        //throw new Exception();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        throw e;
-        //    }
-        //    finally
-        //    {
-        //        //if (connection != null && connection.State == ConnectionState.Open)
-        //        //{ connection.Close(); }
-        //    }
-        //}
+        /// <summary>
+        /// Updates the password hash and salt for a specific user.
+        /// </summary>
+        /// <param name="id">The ID of the user whose password is to be updated.</param>
+        /// <param name="salt">The new salt to be used for the password.</param>
+        /// <param name="hash">The new hash to be used for the password.</param>
+        /// <returns><c>true</c> if the update was successful; otherwise, <c>false</c>.</returns>
+        /// <exception cref="Exception">Thrown when there is an issue with updating the password in the database.</exception>
         public bool UpdatePassword(int id, string salt, string hash)
         {
             string query = "Update Passwords set passwordSalt = @salt, " +
                 "passwordHash = @hash where person_id = @id;";
 
-            //SqlConnection connection = null;
             SqlConnection connection = new SqlConnection(connectionString);
 
             try
@@ -120,24 +96,15 @@ namespace DataAccessLayer
                 connection.Open();
                 SqlCommand command = new SqlCommand(query, connection);
 
-                //SqlCommand command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@id", id);
                 command.Parameters.AddWithValue("@salt", salt);
                 command.Parameters.AddWithValue("@hash", hash);
-                //command.ExecuteScalar();
                 int result = Convert.ToInt32(command.ExecuteScalar());
                 if (result == 0)
                 {
                     return true;
                 }
-                //else
-                //{
                 throw new Exception("Failed to update password \n Please,try again later!");
-                //throw new Exception("An issue occured in the database and password could not be found. \nTry again later!");
-
-                //throw new Exception();
-
-                //}
             }
             catch (Exception ex)
             {
@@ -149,35 +116,29 @@ namespace DataAccessLayer
                 { connection.Close(); }
             }
         }
+
+        /// <summary>
+        /// Adds a new password entry for a specific user.
+        /// </summary>
+        /// <param name="id">The ID of the user for whom the password is being added.</param>
+        /// <param name="salt">The salt for the new password.</param>
+        /// <param name="hash">The hash for the new password.</param>
+        /// <exception cref="Exception">Thrown when there is an issue with adding the password to the database.</exception>
         public void AddPassword(int id, string salt, string hash)
         {
             string query = "Insert into Passwords (person_id,passwordSalt,passwordHash) values (@id,@salt, " +
                 "@hash);";
 
-            //SqlConnection connection = null;
             SqlConnection connection = new SqlConnection(connectionString);
 
             try
             {
                 connection.Open();
                 SqlCommand command = new SqlCommand(query, connection);
-
-                //SqlCommand command = new SqlCommand(query, connection);
                 command.Parameters.AddWithValue("@id", id);
                 command.Parameters.AddWithValue("@salt", salt);
                 command.Parameters.AddWithValue("@hash", hash);
-                //command.ExecuteScalar();
                 int result = Convert.ToInt32(command.ExecuteScalar());
-                //if (result == 0)
-                //{
-                //    return true;
-                //}
-                //else
-                //{
-                //throw new Exception("Failed to add password \n Please,try again later!");
-                //throw new Exception();
-
-                //}
             }
             catch (Exception ex)
             {
